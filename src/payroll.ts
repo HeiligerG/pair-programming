4fb4647420deb8b995f08f3a6bc0902b8916cf38
Pascal Bossert <pascal.bossert@kauz.ch>
2025-04-18 15:05:20 +0200

    feat: LiveCode Implementierung Payroll.ts

--- src/payroll.ts
+++ src/payroll.ts
@@ -24,11 +24,41 @@ export type Payslip = {
 
 export function calculatePayslip(salary: Salary): Payslip {
   // TODO: implement
-  const result: Payslip = {
+
+  const deductions = new Map<string, number>
+  let totalDeductions = 0.0 
+
+  const birthYear = salary.born.getFullYear();
+  const payYear = salary.payday.getFullYear();
+  const age = payYear - birthYear
+  const birthdayThisYear = new Date(payYear, salary.born.getMonth(), salary.born.getDate());
+  const isOver17AfterJan1 = age > 17 || (age === 17 && salary.payday >= birthdayThisYear && salary.payday > new Date(payYear, 0, 1)); 
+
+  const annualSalary = salary.gross * 12;
+
+  if (isOver17AfterJan1) {
+    deductions.set("AHV", (salary.gross * DEDUCTION_RATES.get("AHV") / 100));
+    deductions.set("IV", (salary.gross * DEDUCTION_RATES.get("IV") / 100));
+    deductions.set("EO", (salary.gross * DEDUCTION_RATES.get("EO") / 100));
+  }
+
+  if (annualSalary >= 2500) {
+    deductions.set("ALV", (salary.gross * DEDUCTION_RATES.get("ALV") / 100));
+    deductions.set("NBU", (salary.gross * DEDUCTION_RATES.get("NBU") / 100));
+  }
+
+  if (annualSalary >= 22680) {
+    deductions.set("PK", (salary.gross * DEDUCTION_RATES.get("PK") / 100));
+  }
+
+  deductions.forEach(value => {
+    totalDeductions += value
+  });
+
+  return {
     salary: salary,
-    deductions: new Map(),
-    totalDeductions: 0.0,
-    net: salary.gross,
-  };
-  return result;
+    deductions: deductions,
+    totalDeductions: totalDeductions,
+    net: salary.gross - totalDeductions
+  }
 }
